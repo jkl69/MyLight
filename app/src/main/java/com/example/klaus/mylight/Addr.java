53b0c38e8416c20546431039b8f1b0830188ca28
jaen <jkl69@freenet.de>
2017-02-19 16:57:42 +0100

    v1.1

---
+++ app/src/main/java/com/example/klaus/mylight/Addr.java
@@ -0,0 +1,14 @@
+package com.example.klaus.mylight;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by jaen on 22.11.2016.
+ */
+
+public class Addr {
+
+    static public SharedPreferences pref;
+    static public String IPtext;
+    static public int PORT;
+}
